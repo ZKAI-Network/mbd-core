e31941939d555e3e58aaa00af176829fc284042a
LFG <yl@mbd.xyz>
2025-02-03 18:09:37 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def get_requirements(requirements_file):
 
 setup(
     name="mbd_core",
-    version="0.1.1" + FROZEN_VERSION,
+    version="0.1.2" + FROZEN_VERSION,
     description="""mbd core packages.""",
     author="Yassine Landa",
     author_email="yl@mbd.xyz",
